a8dc6467da279972141c313efcdf175b230f4abe
Allan <allanfelipedk@gmail.com>
2024-06-21 19:39:46 -0300

    feat: listing the topics

---
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -0,0 +1,35 @@
+package br.com.alura.forum.controller
+
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+@RequestMapping("/topicos")
+class TopicoController {
+
+    @GetMapping
+    fun listar(): List<Topico> {
+        val topico = Topico(
+            id = 1,
+            titulo = "Duvida kotlin",
+            mensagem = "Variaveis no kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Allan",
+                email = "allan@gmail.com"
+            ),
+        )
+        return Arrays.asList(topico, topico, topico)
+    }
+
+}
