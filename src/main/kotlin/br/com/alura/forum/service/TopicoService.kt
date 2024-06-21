d4f5e03f27f67f4e4728842908ec2b11396bed69
Allan <allanfelipedk@gmail.com>
2024-06-21 19:54:07 -0300

    feat: searching topic by id

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,23 +7,53 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
-    fun listar(): List<Topico> {
-        val topico = Topico(
+class TopicoService(private var topicos: List<Topico>) {
+    init {
+        val topico1 = Topico(
             id = 1,
-            titulo = "Duvida kotlin",
-            mensagem = "Variaveis no kotlin",
+            titulo = "Duvida kotlin 1",
+            mensagem = "Variaveis no kotlin 1",
             curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação"
+                id = 1, nome = "Kotlin", categoria = "Programação"
             ),
             autor = Usuario(
-                id = 1,
-                nome = "Allan",
-                email = "allan@gmail.com"
+                id = 1, nome = "Allan", email = "allan@gmail.com"
             ),
         )
-        return Arrays.asList(topico, topico, topico)
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvida kotlin 2",
+            mensagem = "Variaveis no kotlin 2",
+            curso = Curso(
+                id = 1, nome = "Kotlin", categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1, nome = "Allan", email = "allan@gmail.com"
+            ),
+        )
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Duvida kotlin 3",
+            mensagem = "Variaveis no kotlin 3",
+            curso = Curso(
+                id = 1, nome = "Kotlin", categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1, nome = "Allan", email = "allan@gmail.com"
+            ),
+        )
+        topicos = Arrays.asList(topico1, topico2, topico3)
     }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorID(id: Long): Topico {
+        return topicos.stream().filter({ t ->
+            t.id == id
+        }).findFirst().get()
+    }
+
+
 }
