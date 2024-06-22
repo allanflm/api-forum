9568f126dd0fa09c04a10a11b85966d5a1c7aecf
Allan <allanfelipedk@gmail.com>
2024-06-21 22:15:35 -0300

    feat: Validation with Bean Validation

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -3,6 +3,7 @@ package br.com.alura.forum.controller
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -15,12 +16,12 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @GetMapping("/{id}")
-    fun buscarPorId(@PathVariable id: Long): TopicoView{
+    fun buscarPorId(@PathVariable id: Long): TopicoView {
         return service.buscarPorID(id)
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody dto: NovoTopicoForm) {
+    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
         service.cadastrar(dto)
 
     }
